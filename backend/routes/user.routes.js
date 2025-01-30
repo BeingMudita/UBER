1e22773aaa4cf2382129d785c2ec6581abbc73eb
Mudita Jain <mudita21jain@gmail.com>
2025-01-30 21:00:25 +0530

    User Authentication Done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {body} = require("express-validator");
 const userController = require("../contoller/user.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register",[
     body("email").isEmail().withMessage("Please enter a valid email"),
@@ -13,6 +14,10 @@ router.post("/login", [
     body("email").isEmail().withMessage("Invalid email"),
     body("password").isLength({min: 6}).withMessage("Wrong password!")
 ], userController.loginUser);
+
+router.get("/profile", authMiddleware.authUser , userController.getUserProfile);
+
+router.get("/logout", authMiddleware.authUser, userController.logoutUser);
 
 
 
