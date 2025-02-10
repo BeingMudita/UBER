a61dceef106951ae9c93b480914c377996f0ec36
Mudita Jain <mudita21jain@gmail.com>
2025-02-10 21:07:05 +0530

    MAPS API

--- backend/services/maps.service.js
+++ backend/services/maps.service.js
@@ -1,7 +1,9 @@
 const axios = require('axios')
+
 module.exports.getAddressCoordinate = async (address) => {
     const apiKey = process.env.MAPS_API;
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${apiKey}`;
+    const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${origin.lng},${origin.ltd};${destination.lng},${destination.ltd}?access_token=${apiKey}&geometries=geojson`;
+
 
     try {
         const response = await axios.get(url);
@@ -13,6 +15,26 @@ module.exports.getAddressCoordinate = async (address) => {
         }
     } catch (error) {
         console.error(error);
+        throw error;
+    }
+};
+
+
+module.exports.getDistanceTime = async (origin, destination) => {
+    try {
+        if (!origin || !destination) {
+            throw new Error("Origin and destination are required");
+        }
+
+        const response = await axios.get(`https://api.mapbox.com/directions/v5/mapbox/driving/${origin.lng},${origin.lat};${destination.lng},${destination.lat}?access_token=${process.env.MAPS_API}`);
+
+        if (response.status !== 200) {
+            throw new Error(`Mapbox API error: ${response.statusText}`);
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error("Error in getDistanceTime:", error.message);
         throw error;
     }
 };
